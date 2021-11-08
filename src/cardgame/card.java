7f3222bec4e48701235f2a184a2d34b3c509ef97
Conan1231 <timcpunkt@googlemail.com>
2021-11-08 18:28:30 +0100

    game part 1 und 2 fertig, es fehlen noch 3 und 4

--- src/cardgame/card.java
+++ src/cardgame/card.java
@@ -7,9 +7,9 @@ public class card {
     int nr;
     ImageIcon pic;
     card(int n){
+        nr = n;
         java.net.URL url=getClass().getResource("./res/card"+nr+".png");
         pic = new ImageIcon(url, "card" + nr);
-
     }
 }
 
--- src/cardgame/mainForm.java
+++ src/cardgame/mainForm.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collections;
 import java.util.Random;
 
 public class mainForm {
@@ -20,6 +21,12 @@ public class mainForm {
     private JLabel pos4;
     private JPanel symbol;
     private JButton restartButton;
+    private JButton shuffleDeckButton;
+    private JRadioButton higherRadioButton;
+    private JRadioButton lowerRadioButton;
+
+    static int game_part = 0;
+    static int latest_nr = 0;
 
     card Cardgame[] = new card[52];
     int pos = 0;
@@ -27,6 +34,7 @@ public class mainForm {
     public mainForm(){
         for(int i = 0; i<52; i++)
             Cardgame[i]=new card(i);
+        shuffle_deck();
         newCardButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -42,14 +50,35 @@ public class mainForm {
         restartButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "not implemented yet, exit now");
-                System.exit(0);
+                //JOptionPane.showMessageDialog(null, "not implemented yet, exit now");
+                //System.exit(0);
+                newCardButton.setEnabled(true);
+                newCardButton.setBackground(Color.white);
+                shuffle_deck();
+                pos1.setIcon(new ImageIcon(getClass().getResource("./res/pos1.png")));
+                pos2.setIcon(new ImageIcon(getClass().getResource("./res/pos2.png")));
+                game_part = 0;
+            }
+        });
+        shuffleDeckButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                shuffle_deck();
             }
         });
     }
 
-    public void mix_card(){
-
+    public void shuffle_deck(){
+        card tmp;
+        for(int i = 0; i<52; i++){
+            Random r = new Random();
+            int nr = r.nextInt(52);
+            tmp = Cardgame[i];
+            Cardgame[i] = Cardgame[nr];
+            Cardgame[nr] = tmp;
+        }
+        newCardButton.setEnabled(true);
+        newCardButton.setBackground(Color.white);
     }
 
     public static void main(String[] args){
@@ -67,9 +96,8 @@ public class mainForm {
         ImageIcon card = Cardgame[pos].pic;
         int nr = Cardgame[pos].nr;
         pos++;
-
         // pos1.setIcon(new ImageIcon(url, "Blaa"));
-        pos1.setIcon(card);
+
         // Abfangen wenn keine Auswahl getroffen
         /*boolean test = true;
         while(test)
@@ -79,15 +107,42 @@ public class mainForm {
         else{
             test = false;
         }*/
-        if ((redRadioButton.isSelected() && nr< 26) || (blackRadioButton.isSelected() && nr >= 26))
-        {
-            JOptionPane.showMessageDialog(null, "nice, your are absolutely right");
+
+        switch(game_part){
+            case 0:
+                pos1.setIcon(card);
+                if ((redRadioButton.isSelected() && nr< 26) || (blackRadioButton.isSelected() && nr >= 26))
+                {
+                    JOptionPane.showMessageDialog(null, "nice, your are absolutely right");
+                    game_part++;
+                    latest_nr = nr%13;
+                    break;
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "wrong. game over!");
+                    newCardButton.setEnabled(false);
+                    newCardButton.setBackground(Color.red);
+                    break;
+                }
+            case 1:
+                pos2.setIcon(card);
+                if ((higherRadioButton.isSelected() && (nr%13)> latest_nr ) || (lowerRadioButton.isSelected() && (nr%13)< latest_nr )){
+                    JOptionPane.showMessageDialog(null, "nice, your are absolutely right");
+                    game_part++;
+                    break;
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "wrong. game over!");
+                    newCardButton.setEnabled(false);
+                    newCardButton.setBackground(Color.red);
+                    break;
+                }
+
+
+
         }
-        else{
-            JOptionPane.showMessageDialog(null, "wrong. game over!");
-            newCardButton.setEnabled(false);
-            newCardButton.setBackground(Color.red);
-        }
+
+
 
     }
 
